358195f5f2ff9e0315f0dab74e0635e910aab7ed
Konstantin Zhukov <moncruist@gmail.com>
2013-12-14 21:42:15 +0400

    Added GPL v3 comments

--- net/http_client.cpp
+++ net/http_client.cpp
@@ -1,10 +1,23 @@
 /*
- * http_client.cpp
+ *  This file is part of Nestor.
  *
- *  Created on: 30 нояб. 2013 г.
- *      Author: moncruist
+ *  Nestor - program for aggregation RSS subscriptions with access via
+ *  IMAP interface.
+ *  Copyright (C) 2013  Konstantin Zhukov moncruist@gmail.com
+ *
+ *  Nestor is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Nestor is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see {http://www.gnu.org/licenses/}.
  */
-
 #include "http_client.h"
 #include <string>
 #include <iostream>
