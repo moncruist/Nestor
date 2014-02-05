3d4a803937c75069616ddfcc7dac18abc5d9e78f
Konstantin Zhukov <konstantin.zhukov@moncruist.com>
2014-02-05 20:30:22 +0400

    Added license headers to the /rss source files.

--- rss/rss_object.h
+++ rss/rss_object.h
@@ -1,10 +1,23 @@
 /*
- * rss_object.h
+ *  This file is part of Nestor.
  *
- *  Created on: 14 дек. 2013 г.
- *      Author: moncruist
+ *  Nestor - program for aggregation RSS subscriptions providing
+ *  access via IMAP interface.
+ *  Copyright (C) 2013-2014  Konstantin Zhukov
+ *
+ *  Nestor is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Nestor is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see {http://www.gnu.org/licenses/}.
  */
-
 #ifndef RSS_OBJECT_H_
 #define RSS_OBJECT_H_
 
