ba5c7f0efaf19d9c767b5e53aae18af520a18b63
Konstantin Zhukov <konstantin.zhukov@moncruist.com>
2014-04-07 23:01:40 +0400

    Added LGPL License text into source files.

--- tests/imap_string_test.h
+++ tests/imap_string_test.h
@@ -1,3 +1,24 @@
+/*
+ *  This file is part of Nestor.
+ *
+ *  Nestor - program for aggregation RSS subscriptions providing
+ *  access via IMAP interface.
+ *  Copyright (C) 2013-2014  Konstantin Zhukov
+ *
+ *  Nestor is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Nestor is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see {http://www.gnu.org/licenses/}.
+ */
+
 #ifndef IMAP_STRING_TEST_H_
 #define IMAP_STRING_TEST_H_
 
